5721988661bb269e391b8ff041f2c4b9dd158847
keelera98 <32946655+keelera98@users.noreply.github.com>
2018-10-20 16:55:15 -0400

    out of date

--- Launcher.java
+++
@@ -1,8 +0,0 @@
-public class Launcher {
-    public static void main(String args[]){
-        //Display dis = new Display("test", 800, 300);
-        Game game = new Game("test", 400, 400);
-        game.start();
-
-    }
-}
